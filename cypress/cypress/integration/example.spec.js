c892c84d76f6477914f74946982390bb657aaa0a
Jack Mahoney <jackmahoney>
2019-09-19 17:58:57 +0200

    readme

--- cypress/cypress/integration/example.spec.js
+++ cypress/cypress/integration/example.spec.js
@@ -5,9 +5,12 @@ describe('Sign up', () => {
   context('Example sign up page', () => {
     it('can generate a new email address to test sign up', () => {
       cy.newEmailAddress().then(({ emailAddress }) => {
-        cy.visit('https://example.cypress.io/commands/actions');
+        cy.visit('https://twitter.com/i/flow/signup');
+        cy.contains('email instead').click();
+        cy.screenshot('login');
         cy.get('input[type=email]').type(emailAddress);
         cy.get('input[type=email]').should('contain.value', '@mailslurp.com');
+        cy.screenshot('completed');
         // submit the form, get the verification code, verify the user (see docs for those examples)
       });
     });
