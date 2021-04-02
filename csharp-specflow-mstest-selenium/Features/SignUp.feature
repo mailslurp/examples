2b9db6e1abd803cd85db40fb1cabe20b5bcd9a6b
juxedo <jackmahoney212@gmail.com>
2021-04-02 20:43:14 +1300

    add_specflow_wip

---
+++ csharp-specflow-mstest-selenium/Features/SignUp.feature
@@ -0,0 +1,9 @@
+﻿Feature: SignUp
+Create a MailSlurp email address and sign up for a demo application. Receive a confirmation code by email and verify the account. Login to the web app and see a happy dog.
+
+@signup
+Scenario: User sign up and email verification
+	Given a user visits the demo app
+	And has a test email address
+	When the user signs up
+	Then they receive a confirmation code by email and can verify their account
---
+++ csharp-specflow-mstest-selenium/Steps/SignUpStepDefinitions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecflowSeleniumExample.Steps
+{
+    [Binding]
+    public sealed class SignUpStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public SignUpStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("a user visits the demo app")]
+        public void GivenTheFirstNumberIs()
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            throw new ArgumentException("Boom");
+        }
+
+        [Given("has a test email address")]
+        public void GivenTheSecondNumberIs()
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            _scenarioContext.Pending();
+        }
+
+        [When("the user signs up")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            //TODO: implement act (action) logic
+
+            _scenarioContext.Pending();
+        }
+
+        [Then("they receive a confirmation code by email and can verify their account")]
+        public void ThenTheResultShouldBe()
+        {
+            //TODO: implement assert (verification) logic
+
+            _scenarioContext.Pending();
+        }
+    }
+}
