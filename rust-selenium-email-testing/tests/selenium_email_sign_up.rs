4eb21cf4064a34fc9073f6136c42cedacdcec362
Wusi <jack@mailslurp.com>
2021-04-10 08:42:30 +0200

    server

--- rust-selenium-email-testing/tests/selenium_email_sign_up.rs
+++ rust-selenium-email-testing/tests/selenium_email_sign_up.rs
@@ -8,15 +8,37 @@ mod tests {
     use tokio;
     use mailslurp::apis::inbox_controller_api::SendEmailAndConfirmParams;
     use mailslurp::apis::wait_for_controller_api;
+    use std::time::Duration;
+    use reqwest::Client;
+    use thirtyfour::http::reqwest_async::ReqwestDriverAsync;
+    use thirtyfour::GenericWebDriver;
+    use color_eyre::Report;
+
+    const TIMEOUT: Duration = Duration::from_millis(60_000);
 
     #[tokio::test]
-    async fn my_test() -> color_eyre::Result<()> {
+    async fn run_test() -> color_eyre::Result<()> {
         // use color-eyre for better result reports
         color_eyre::install()?;
 
-        let client = reqwest::Client::new();
+        // setup driver (selenium with geckodriver should be running)
+        let caps = DesiredCapabilities::firefox();
+        let driver: GenericWebDriver<ReqwestDriverAsync> = WebDriver::new_with_timeout(
+            "http://localhost:4444/wd/hub",
+            &caps,
+            Some(TIMEOUT)
+        ).await?;
+
+
+        // allow a long timeout to wait for emails to arrive
+        let client: Client = reqwest::ClientBuilder::new()
+            .timeout(TIMEOUT)
+            .connect_timeout(TIMEOUT)
+            .build()?;
+
         // read mailslurp api key from environment variable
-        let api_key = env::var("API_KEY").ok().unwrap();
+        let api_key: String = env::var("API_KEY")?;
+
         // configure mailslurp with base path, api key, and reqwest client
         let configuration = configuration::Configuration {
             // required fields
@@ -48,31 +70,8 @@ mod tests {
         let inbox = inbox_controller_api::create_inbox(&configuration, create_inbox_params).await.ok().unwrap();
         assert!(inbox.email_address.unwrap().contains("@mailslurp"));
 
-
-        // // setup driver (selenium with geckodriver should be running)
-        // let caps = DesiredCapabilities::firefox();
-        // let driver = WebDriver::new("http://localhost:4444/wd/hub", &caps).await?;
-        //
-        // // load playground application
-        // driver.get("https://playground.mailslurp.com").await?;
-        // let elem_form = driver.find_element(By::Id("search-form")).await?;
-        //
-        // // Find element from element.
-        // let elem_text = elem_form.find_element(By::Id("searchInput")).await?;
-        //
-        // // Type in the search terms.
-        // elem_text.send_keys("selenium").await?;
-        //
-        // // Click the search button.
-        // let elem_button = elem_form
-        //     .find_element(By::Css("button[type='submit']"))
-        //     .await?;
-        // elem_button.click().await?;
-        //
-        // // Look for header to implicitly wait for the page to load.
-        // driver.find_element(By::ClassName("firstHeading")).await?;
-        // assert_eq!(driver.title().await?, "Selenium - Wikipedia");
-        //
+        // load playground application
+        driver.get("https://playground.mailslurp.com").await?;
         Ok(())
     }
 }
