0267722b36feb6a06461622a27b07fb78b891082
jackmahoney <jackmahoney@jackmahoneys-MacBook-Pro.local>
2022-07-21 11:19:35 +1200

    add-laravel

---
+++ php-laravel-email-examples/resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<main class="container">
+<h1>Laravel Email Demonstration</h1>
+<p>Use email in PHP</p>
+</main>
+</body>
+</html>
