1d3a019ce2931b1c9759a6fdde7509b4529426e4
jackmahoney <contact@mailslurp.dev>
2022-07-22 22:50:36 +1200

    fsharp-tests

--- fsharp-email-mstest/fsharp-email-mstest/Tests.fs
+++ fsharp-email-mstest/fsharp-email-mstest/Tests.fs
@@ -2,10 +2,15 @@ namespace fsharp_email_mstest
 
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
+open mailslurp.Client
 
 [<TestClass>]
 type TestClass () =
 
     [<TestMethod>]
     member this.TestMethodPassing () =
-        Assert.IsTrue(true);
+        let apiKey = Environment.GetEnvironmentVariable("API_KEY")
+        Assert.IsNotNull(apiKey)
+        let config = Configuration()
+        config.ApiKey.Add("x-api-key", apiKey);
+        Assert.IsTrue(true)
