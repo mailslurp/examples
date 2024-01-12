2d75a01cb62532cc9271ecadac956f50db53e56c
jackmahoney <jackmahoney212@gmail.com>
2024-01-12 14:08:21 +1300

    fix-example-code

--- javascript-cypress-mailslurp-plugin/cypress/e2e/integration-test.cy.ts
+++ javascript-cypress-mailslurp-plugin/cypress/e2e/integration-test.cy.ts
@@ -15,57 +15,56 @@ describe("user sign up test with mailslurp plugin", function () {
             })
     });
     //</gen>
-    //<gen>cypress_plugin_01
     it("01 - can load the demo application", function () {
+        //<gen>cypress_plugin_01
         // get wrapped email address and assert contains a mailslurp email address
         expect(this.emailAddress).to.contain("@mailslurp");
         // visit the demo application
         cy.visit("https://playground.mailslurp.com")
         cy.title().should('contain', 'React App');
+        //</gen>
+        //<gen>cypress_plugin_02
+        cy.then(function () {
+            // click sign up and fill out the form
+            cy.get("[data-test=sign-in-create-account-link]").click()
+            // use the email address and a test password
+            cy.get("[name=email]").type(this.emailAddress).trigger('change');
+            cy.get("[name=password]").type('test-password').trigger('change');
+            // click the submit button
+            cy.get("[data-test=sign-up-create-account-button]").click();
+            //</gen>
+        })
+        //<gen>cypress_plugin_03
+        cy.then(function () {
+            // app will send user an email containing a code, use mailslurp to wait for the latest email
+            cy.mailslurp()
+                // use inbox id and a timeout of 30 seconds
+                .then(mailslurp => mailslurp.waitForLatestEmail(this.inboxId, 30000, true))
+                // extract the confirmation code from the email body
+                .then(email => /.*verification code is (\d{6}).*/.exec(email.body!!)!![1])
+                // fill out the confirmation form and submit
+                .then(code => {
+                    cy.get("[name=code]").type(code).trigger('change');
+                    cy.get("[data-test=confirm-sign-up-confirm-button]").click();
+                })
+        })
+        //</gen>
+        //<gen>cypress_plugin_04
+        // fill out sign in form
+        cy.then( function () {
+            // use the email address and a test password
+            cy.get("[data-test=username-input]").type(this.emailAddress).trigger('change');
+            cy.get("[data-test=sign-in-password-input]").type('test-password').trigger('change');
+            // click the submit button
+            cy.get("[data-test=sign-in-sign-in-button]").click();
+        });
+        //</gen>
+        //<gen>cypress_plugin_05
+        // can see authorized welcome screen
+        cy.then(function () {
+            // click sign up and fill out the form
+            cy.get("h1").should("contain", "Welcome");
+        })
+        //</gen>
     });
-    //</gen>
-    //<gen>cypress_plugin_02
-    // use function instead of arrow syntax to access aliased values on this
-    it("02 - can sign up using email address", function () {
-        // click sign up and fill out the form
-        cy.get("[data-test=sign-in-create-account-link]").click()
-        // use the email address and a test password
-        cy.get("[name=email]").type(this.emailAddress).trigger('change');
-        cy.get("[name=password]").type('test-password').trigger('change');
-        // click the submit button
-        cy.get("[data-test=sign-up-create-account-button]").click();
-    });
-    //</gen>
-    //<gen>cypress_plugin_03
-    it("03 - can receive confirmation code by email", function () {
-        // app will send user an email containing a code, use mailslurp to wait for the latest email
-        cy.mailslurp()
-            // use inbox id and a timeout of 30 seconds
-            .then(mailslurp => mailslurp.waitForLatestEmail(this.inboxId, 30000, true))
-            // extract the confirmation code from the email body
-            .then(email => /.*verification code is (\d{6}).*/.exec(email.body!!)!![1])
-            // fill out the confirmation form and submit
-            .then(code => {
-                cy.get("[name=code]").type(code).trigger('change');
-                cy.get("[data-test=confirm-sign-up-confirm-button]").click();
-            })
-    });
-    //</gen>
-    //<gen>cypress_plugin_04
-    // fill out sign in form
-    it("04 - can sign in with confirmed account", function () {
-        // use the email address and a test password
-        cy.get("[data-test=username-input]").type(this.emailAddress).trigger('change');
-        cy.get("[data-test=sign-in-password-input]").type('test-password').trigger('change');
-        // click the submit button
-        cy.get("[data-test=sign-in-sign-in-button]").click();
-    });
-    //</gen>
-    //<gen>cypress_plugin_05
-    // can see authorized welcome screen
-    it("05 - can see welcome screen", function () {
-        // click sign up and fill out the form
-        cy.get("h1").should("contain", "Welcome");
-    });
-    //</gen>
 });
