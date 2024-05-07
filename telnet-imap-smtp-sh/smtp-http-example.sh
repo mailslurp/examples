0e73e0a6a7c6dfebce50aa0b4aa9840eaa381a4d
jackmahoney <jackmahoney212@gmail.com>
2024-05-07 20:09:04 +1200

    http-example

--- telnet-imap-smtp-sh/smtp-http-example.sh
+++ telnet-imap-smtp-sh/smtp-http-example.sh
@@ -13,11 +13,9 @@ export USERNAME=$(echo "$ACCESS" | jq -j '.smtpUsername')
 export PASSWORD=$(echo "$ACCESS" | jq -j '.smtpPassword')
 export PORT=$(echo "$ACCESS" | jq -j '.smtpServerPort')
 export HOST=$(echo "$ACCESS" | jq -j '.smtpServerHost')
-#<gen>telnet_smtp_auth_plain_encoding
 auth_string="\0$USERNAME\0$PASSWORD"
 base64_auth=$(echo -ne "$auth_string" | base64)
 auth_plain_command="AUTH PLAIN $base64_auth"
-#</gen>
 export USERNAME_PASSWORD_BASE64=$base64_auth
 # execute the expect test to connect to the server using the set variables
 ./smtp-example.exp
